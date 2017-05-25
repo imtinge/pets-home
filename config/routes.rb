eca587759872a4c2a1b55623efe0279119e94683
imtinge <imtinge@outlook.com>
2017-05-25 19:44:20 +0800

    remove uptoken route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-  get '/uptoken', to: 'posts#uptoken', as: 'uptoken'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
