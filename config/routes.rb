c9d2bd304162d86577eef65fd40291ff3136974e
xy <ttjxy@gmail.com>
2017-05-25 12:13:48 +0800

    网页样式

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'posts#new'
+  root 'welcome#index'
   resources :posts
   get '/uptoken', to: 'posts#uptoken', as: 'uptoken'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
