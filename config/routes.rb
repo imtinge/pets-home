fb31970b3992554d444c8cd36b07095eed36aef1
imtinge <imtinge@outlook.com>
2017-05-25 19:43:25 +0800

    drop scaffold posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
-  resources :posts
   get '/uptoken', to: 'posts#uptoken', as: 'uptoken'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
