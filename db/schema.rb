eb89d6e42ff72516cc2968a546c2115cb390dc51
imtinge <imtinge@outlook.com>
2017-05-28 21:22:06 +0800

    posts init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526081840) do
+ActiveRecord::Schema.define(version: 20170528130651) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "reporters", force: :cascade do |t|
     t.string "pet_name"
